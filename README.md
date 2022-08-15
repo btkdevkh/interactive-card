3c2e40959ee0d15263e81f2202500e9ce8d0c159
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-08-15 21:29:27 +0200

    interactive card challenge done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Challenge Done !!!
 # Frontend Mentor - Interactive card details form
 
 ![Design preview for the Interactive card details form coding challenge](./design/desktop-preview.jpg)
--- main.js
+++ main.js
@@ -1,41 +1,52 @@
-const form = document.querySelector('[data-form="data-form"]')
-
+const form = document.querySelector('#form')
+const cardComplete = document.querySelector('#card-complete')
 const cardHolderName = document.querySelector('.card-holder-name')
 const cardNumber = document.querySelector('.card-number')
 const cardExpDateMonth = document.querySelector('.card-exp-date-month')
 const cardExpDateYear = document.querySelector('.card-exp-date-year')
 const cardCVC = document.querySelector('.card-cvc')
 
-const cardHeader = document.querySelector('.card-header')
-const aside = document.querySelector('.card-form-sidebar')
-const card = document.querySelector('.card-header .container')
-
 const cardHolderNameInput = document.querySelector('#card-holder-name')
 const cardNumberInput = document.querySelector('#card-number')
 const cardExpDateMonthInput = document.querySelector('#card-exp-date-month')
 const cardExpDateYearInput = document.querySelector('#card-exp-date-year')
+const cardCVCInput = document.querySelector('#card-cvc')
 
+const cardHeader = document.querySelector('.card-header')
+const aside = document.querySelector('.card-form-sidebar')
+const card = document.querySelector('.card-header .container')
+
+// States
 let fullName = ''
 let cardNum = ''
 let cardExpMonth = ''
 let cardExpYear = ''
+let cardCVCNum = ''
 
+// Stuffs with Media Queries
+window.addEventListener('DOMContentLoaded', () => {
+  if (window.matchMedia("(min-width: 920px)").matches) {
+    aside.append(card)
+  } else {
+    cardHeader.append(card)
+  }
+})
 window.addEventListener('resize', (e) => {
   if(e.target.innerWidth >= 920) {
-    card.remove()
     aside.append(card)
   } else {
     cardHeader.append(card)
   }
 })
 
+// Inputs
 cardHolderNameInput.addEventListener('input', () => {
   cardHolderName.innerText = `${cardHolderNameInput.value}`
   fullName = cardHolderNameInput.value
 })
 
 cardNumberInput.addEventListener('input', () => {
-  const reg = /^[\d]{16}$/
+  const reg = /^([\d\ ]{5}){3}([\d]{4})$/
 
   if(!reg.test(cardNumberInput.value)) {
     cardNumberInput.classList.add('invalid')
@@ -48,7 +59,6 @@ cardNumberInput.addEventListener('input', () => {
 })
 
 cardExpDateMonthInput.addEventListener('input', () => {
-  console.log(123);
   const reg = /^[\d]{2}$/
 
   if(!reg.test(cardExpDateMonthInput.value)) {
@@ -74,6 +84,36 @@ cardExpDateYearInput.addEventListener('input', () => {
   cardExpYear = cardExpDateYearInput.value
 })
 
+cardCVCInput.addEventListener('input', () => {
+  const reg = /^[\d]{3}$/
+
+  if(!reg.test(cardCVCInput.value)) {
+    cardCVCInput.classList.add('invalid')
+  } else {
+    cardCVCInput.classList.remove('invalid')
+  }
+
+  cardCVC.innerText = `${cardCVCInput.value}`
+  cardCVCNum = cardCVCInput.value
+})
+
+// Submit
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+
+  if(!fullName || !cardNum || !cardExpDateMonth || !cardExpDateYear || !cardCVCNum) {
+    return
+  }
+
+  const card = {
+    fullName: fullName.trim(),
+    cardNum: cardNum.replace(/\s/g, ''),
+    cardExpMonth: cardExpMonth.trim(),
+    cardExpYear: cardExpYear.trim(),
+    cardCVCNum: cardCVCNum.trim()
+  }
+  // console.log(card);
+
+  cardComplete.classList.add('active')
+  form.classList.add('active')
 })
